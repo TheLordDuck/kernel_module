5fec51e566b4bc041fabda865fc5583e1bcd7ca0
zephyr <45296195+zephvr@users.noreply.github.com>
2021-10-26 22:54:10 +0200

    style: translate echo

--- boot.sh
+++ boot.sh
@@ -6,7 +6,7 @@ if which $QEMU
 then
 	$QEMU $* -boot c -kernel linux-4.19.206/arch/x86/boot/bzImage -hda ./image.img -append "root=/dev/hda clock=pit" -net nic,model=e1000 -net user -k es
 else
-	echo No he trobat executable qemu / No he encontrado ejecutable qemu
-	echo Està instal.lat? / ¿Está instalado?
-	echo Està actualitzada la variable PATH? / ¿Está actualizada la variable PATH?
+	echo qemu not found
+	echo Is it installed ?
+	echo check your PATH
 fi
